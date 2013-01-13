294450f3eefa4df51ba266cfecea3b98957b89a9
Stefan Neculai <stefan.neculai@gmail.com>
2013-01-13 17:17:06 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeDirectory::Application.config.secret_token = '6c4b7f0509ed33b1f697b4642513ffe68757b30597b7a3bb04acde8edb1fa14404dc84daae13c10bfb3704bccf0d45fbbf473bb4064e0e2cf8d50cd4cdeb651f'
